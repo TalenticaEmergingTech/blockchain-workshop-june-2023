c7acb993e495a1e3d964e1a85841232b6b8e74ab
ankitwasnik <ankitwasnik7@gmail.com>
2023-06-14 20:11:28 +0530

    Add minimal code

--- smart-contract/contracts/SimpleLottery.sol
+++ smart-contract/contracts/SimpleLottery.sol
@@ -6,38 +6,44 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 // There can be only 1 lottery at a time. 
-// Owner of this lottery should be able to start a lottery if there is no running lottery. 
+// Owner of this lottery should be able to start a lottery if there is no actively running lottery. 
 // To start a lottery, owner has to provide the price of the ticket, max allowed participants, winner prize
-// Owner has to declare the winner once the requied number of participants has participated in the lottery. 
+// Owner has to declare the winner once the requied number of participants have participated in the lottery.
+// Anyone should be able to check the details of the lottery and the tickets.
+// This contract is importing Ownable contract. Ownable contract provides onlyOwner modifier --> https://docs.openzeppelin.com/contracts/4.x/access-control
 contract SimpleLottery is Ownable {
 
     struct Lottery {
         uint256 id;
         uint256 maxParticipants;
-        uint256 ticketPrice;        
-        uint256 winningAmount;      
+        uint256 ticketPrice;        // Price is in wei( 1 eth = 1e18 wei ).
+        uint256 winningAmount;      // winner will winning amount in wei
         LotteryStatus status;
         string winnerTicketId; 
         uint256 ticketsSold;       // number of tickets sold
     }
 
     enum LotteryStatus {
-        NOT_STARTED,    
+        NOT_STARTED,        // Initial state i.e. no lottery has been started  
         STARTED,            // Users can participate
         CLOSED,             // No more participants allowed
         WINNER_DECLARED     // Winner declared, lottery can be started again       
     }
 
+    // Tickets starts with 0 for each lottery 
     struct Ticket {
-        string id;
+        string id;          // id = lotteryId + "_" + ticketId                    
         uint256 lotteryId;
-        address owner;
+        address owner;      
     }
 
 
     uint256 public currentLotteryId;
+    // Lottery id to Lottery struct map
     mapping(uint256 => Lottery) public lotteryMap;
+    // Ticket id to ticket struct map
     mapping(string => Ticket) public ticketMap;
+    // Used for pseudo random function
     uint256 private seed;
 
     constructor() {
@@ -45,69 +51,49 @@ contract SimpleLottery is Ownable {
         seed = (block.timestamp + block.prevrandao) % 100;
     }
 
+    // Events
     event LotteryStarted(uint256 id, uint256 maxParticipants, uint256 ticketPrice, uint256 winningAmount);
     event TicketBought(uint256 lotteryId, string ticketId, address by);
     event LotteryFull(uint256 lotteryId);
     event WinnerDeclared(uint256 lotteryId, string ticketId, address owner, uint256 winningAmount);
 
-    function start(uint256 _maxParticipants, uint256 _ticketPrice, uint256 _winningAmount) public canStart onlyOwner returns (uint256) {
-        // validate that a lottery is not active
-        currentLotteryId++;
-        Lottery memory lottery = Lottery({
-            id: currentLotteryId,
-            maxParticipants: _maxParticipants,
-            ticketPrice: _ticketPrice,
-            winningAmount: _winningAmount,
-            status: LotteryStatus.STARTED,
-            winnerTicketId: "",
-            ticketsSold: 0
-        });
-        lotteryMap[currentLotteryId] = lottery;
-
-        emit LotteryStarted(lottery.id, lottery.maxParticipants, lottery.ticketPrice, lottery.winningAmount);
-        return currentLotteryId;
+    // Owner should be able to start a lottery if there is no actively running lottery. 
+    // Also, should be able to start a new lottery is the previous lottery winner has been declared.  
+    function start(uint256 _maxParticipants, uint256 _ticketPrice, uint256 _winningAmount) public returns (uint256 lotteryId) {
+        return 0;
     }
 
-    function buy() public payable buyAllowed returns (string memory ticketId) {
-        // validate lottery is active
-        Lottery storage lottery = lotteryMap[currentLotteryId];
-        require(msg.value == lottery.ticketPrice, "Value does not equals the ticket price");
-        lottery.ticketsSold++;
-        
-        if (lottery.ticketsSold == lottery.maxParticipants) {
-            lottery.status = LotteryStatus.CLOSED;
-        }
-        Ticket memory ticket = Ticket(getTicketId(lottery.ticketsSold), currentLotteryId, msg.sender);
-        ticketMap[ticket.id] = ticket;
-
-        emit TicketBought(lottery.id, ticket.id, ticket.owner);
-
-        return ticket.id;
+    // Anyone can buy the ticket if the current lottery is open
+    // The msg.value should equal the price of the ticket.
+    // Buyer will get a ticket id.
+    function buy() public returns (string memory ticketId) {
+        // TODO: Implement this function
+        return "";
     }
 
+    // Owner can declare the winner if the lottery has reached the required number of participants
+    // Transfer the winning amount to the ticket owner
+    function declareWinner() public returns (string memory winnerTicketId) {
+        // TODO: Implement this function
+        return "";
+    }
+
+    //E.g. for lottery no 2 and ticket no 5, ticketId = 2_5
     function getTicketId(uint256 _ticketNo) private view returns (string memory) {
         return string.concat(Strings.toString(currentLotteryId) , "_", Strings.toString(_ticketNo));
     }
 
-    function declareWinner() public onlyOwner isLotteryFull returns (string memory winner) {
-        Lottery storage lottery = lotteryMap[currentLotteryId];
-        uint256 winnerTicketNo = getRandomNumber(lottery.maxParticipants) + 1;
-        lottery.winnerTicketId = getTicketId(winnerTicketNo);
-        lottery.status = LotteryStatus.WINNER_DECLARED;
-        address winnerAddress = ticketMap[lottery.winnerTicketId].owner;
-
-        emit WinnerDeclared(lottery.id, lottery.winnerTicketId, winnerAddress, lottery.winningAmount);
-        payable(winnerAddress).transfer(lottery.winningAmount);
-        return lottery.winnerTicketId;
+    // Gets the contract balance
+    function getBalance() public view returns (uint256) {
+        // TODO: Implement this function
+        return 0;
     }
 
+    // This is a pseudo random number generation code. Do not use it in production
+    // Refer https://docs.chain.link/getting-started/intermediates-tutorial for a production grade random function
     function getRandomNumber(uint256 _range) private returns(uint256) {
         seed = (seed + block.timestamp + block.prevrandao) % _range;
         return seed;
-    }
-
-    function getBalance() public view returns (uint256) {
-        return address(this).balance;
     }
 
     modifier canStart {
@@ -127,6 +113,5 @@ contract SimpleLottery is Ownable {
         require(lottery.status == LotteryStatus.CLOSED, "Lottery is not full!");
         _;
     }
-
 
 }
