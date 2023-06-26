ad23b1c64f60da0312516f210d29dfc70b49b1ac
ankitwasnik <ankitwasnik7@gmail.com>
2023-06-26 15:44:30 +0530

    Add polygon mumbai support

--- smart-contract/hardhat.config.js
+++ smart-contract/hardhat.config.js
@@ -2,7 +2,7 @@ require("@nomicfoundation/hardhat-toolbox");
 const dotenv = require("dotenv");
 const path = require('path');
 require('dotenv').config();
-const { SEPOLIA_ALCHEMY_URL, PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
+const { SEPOLIA_ALCHEMY_URL, PRIVATE_KEY, SCAN_API_KEY, POLYGON_MUMBAI_ALCHEMY_URL } = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -13,9 +13,13 @@ module.exports = {
     sepolia: {
       url: SEPOLIA_ALCHEMY_URL || "https://eth-sepolia.g.alchemy.com/v2/",
       accounts: [PRIVATE_KEY || "0000000000000000000000000000000000000000000000000000000000000000"]
+    },
+    polygon_mumbai: {
+      url: POLYGON_MUMBAI_ALCHEMY_URL || "https://polygon-mumbai.g.alchemy.com/v2",
+      accounts: [PRIVATE_KEY || "0000000000000000000000000000000000000000000000000000000000000000"]  
     }
   },
   etherscan: {
-    apiKey: ETHERSCAN_API_KEY || "0000000000000000000000000000000000",
+    apiKey: SCAN_API_KEY || "0000000000000000000000000000000000",
   },
 };
